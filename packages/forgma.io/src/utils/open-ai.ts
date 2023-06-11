73a94835f55a16334ef152621e5c526c1dc153df
John Dunning <fw@johndunning.com>
2023-06-10 19:27:36 -0700

    Add DigitalOcean URL for generateKeys
    Pass array of panels in a panels key in the request.

--- packages/forgma.io/src/utils/open-ai.ts
+++ packages/forgma.io/src/utils/open-ai.ts
@@ -1,11 +1,12 @@
 import { FormioJSON } from "@/types";
 
-const GenerateKeysURL = "http://127.0.0.1:3000/api/generateKeys";
+//const GenerateKeysURL = "http://127.0.0.1:3000/api/generateKeys";
+const GenerateKeysURL = "https://faas-sfo3-7872a1dd.doserverless.co/api/v1/web/fn-ebdb2c50-b3cd-475a-a51d-2cf90d5b6185/openai/generateKeys";
 
 export async function generateKeys(
-	components: FormioJSON[]): Promise<FormioJSON[]>
+	panels: FormioJSON[]): Promise<FormioJSON[]>
 {
-	const body = JSON.stringify(components);
+	const body = JSON.stringify({ panels });
 	const response = await fetch(GenerateKeysURL, {
 		method: "POST",
 		headers: {
